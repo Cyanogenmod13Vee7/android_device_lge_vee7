d85c79b4278759db92a754d25631aeeabf195c64
neutrondev <neutrondev03@gmail.com>
2016-02-28 15:37:19 +0200

    Fixing bluetooth

--- libbt-vendor/Android.mk
+++ libbt-vendor/Android.mk
@@ -15,17 +15,15 @@
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
 #
 
-BDROID_DIR:= external/bluetooth/bluedroid
+BDROID_DIR:= system/bt
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libbt-vendor
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_OWNER := qcom
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 
 LOCAL_SRC_FILES := \
 	bt_vendor_qcom.c \
