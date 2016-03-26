c76836fe93d0c6b337bc08b0ed5add1e5a8f3f39
neutrondev <neutrondev03@gmail.com>
2016-03-26 17:42:30 +0200

    Sensor v1

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -1,5 +1,1 @@
-ifneq ($(filter titan thea, $(TARGET_DEVICE)),)
-include $(call all-named-subdir-makefiles,bst)
-else ifneq ($(filter falcon peregrine, $(TARGET_DEVICE)),)
-include $(call all-named-subdir-makefiles, falcon-peregrine)
-endif
+include $(all-subdir-makefiles)
